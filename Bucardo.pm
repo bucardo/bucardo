3ae35b5e5987cd979ded9f89f6e4d88d6a166ea3
Greg Sabino Mullane <greg@endpoint.com>
2008-11-18 22:03:34 -0500

    Bump version, update Changes file.

--- Bucardo.pm
+++ Bucardo.pm
@@ -9,7 +9,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '3.2.0';
+our $VERSION = '3.2.1';
 
 ## Begin Moose classes
 {
@@ -5317,7 +5317,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This documents describes Bucardo version 3.2.0
+This documents describes Bucardo version 3.2.1
 
 =head1 SYNOPSIS
 
