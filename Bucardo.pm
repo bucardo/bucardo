3d132773527b6a4723d4b5b5d318ac49799f24d8
Greg Sabino Mullane <greg@endpoint.com>
2009-07-29 11:06:00 -0400

    Bump version to 4.0.0

--- Bucardo.pm
+++ Bucardo.pm
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '3.2.7';
+our $VERSION = '4.0.0';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -5506,7 +5506,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 3.2.7 of Bucardo
+This document describes version 4.0.0 of Bucardo
 
 =head1 SYNOPSIS
 
