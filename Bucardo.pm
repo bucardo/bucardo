4462eecc530a7191fbefdcd32852ee9df0772e1b
Greg Sabino Mullane <greg@endpoint.com>
2009-09-25 08:30:10 -0400

    Fix typo.

--- Bucardo.pm
+++ Bucardo.pm
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '4.1.1';
+our $VERSION = '4.1.2';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -4323,7 +4323,7 @@ sub start_kid {
 
 				## Handle sequences first, by simply forcing a setval
 				if ($g->{reltype} eq 'sequence') {
-					$SQL = "SELECT last_value, iscalled FROM $S.$T";
+					$SQL = "SELECT last_value, is_called FROM $S.$T";
 					my ($lastval, $iscalled) = @{$sourcedbh->selectall_arrayref($SQL)->[0]};
 
 					$self->glog("Setting sequence $S.$T to value of $lastval, is_called is $iscalled");
@@ -5910,7 +5910,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.1.1 of Bucardo
+This document describes version 4.1.2 of Bucardo
 
 =head1 SYNOPSIS
 
