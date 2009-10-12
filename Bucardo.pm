3c05b42d9c5962cd9baae3f50ef7d29dd7614d6c
Greg Sabino Mullane <greg@endpoint.com>
2009-10-12 11:09:56 -0400

    Bump version to 4.4.0, due to the changed customselect behavior.

--- Bucardo.pm
+++ Bucardo.pm
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '4.3.0';
+our $VERSION = '4.4.0';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -6200,7 +6200,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.3.0 of Bucardo
+This document describes version 4.4.0 of Bucardo
 
 =head1 WEBSITE
 
