473b274af7281f65d98f3aff3ac78873960e7dd5
Greg Sabino Mullane <greg@endpoint.com>
2010-06-15 10:18:58 -0400

    Typo.

--- Bucardo.pm
+++ Bucardo.pm
@@ -6251,7 +6251,7 @@ sub cleanup_kid {
 
     my ($self,$reason,$extrainfo) = @_;
 
-    $self->glog("Kid $$ exiting< at cleanup_kid. $extrainfo Reason: $reason", 5);
+    $self->glog("Kid $$ exiting at cleanup_kid. $extrainfo Reason: $reason", 5);
 
     ## Remove the pid file
     if (unlink $self->{KIDPIDFILE}) {
