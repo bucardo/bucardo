9e576f0f8a45749970ede892122be3f4f4e295e5
Greg Sabino Mullane <greg@endpoint.com>
2011-04-14 14:07:30 -0400

    Make sure we commit to target before master, to fix rare race condition. Bump version to 4.4.3

--- Bucardo.pm
+++ Bucardo.pm
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '4.4.2';
+our $VERSION = '4.4.3';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -5999,8 +5999,8 @@ sub start_kid {
         }
         else {
             $self->glog('Issuing final commit for source and target');
-            $sourcedbh->commit();
             $targetdbh->commit();
+            $sourcedbh->commit();
             if ($sync->{usecustomselect}) {
                 for my $g (@$goatlist) {
                     next if ! $g->{cs_temptable};
@@ -6233,7 +6233,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.4.2 of Bucardo
+This document describes version 4.4.3 of Bucardo
 
 =head1 WEBSITE
 
