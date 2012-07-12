bfe2ef72adaac4580f086a3dd2724dc09a8f595c
Greg Sabino Mullane <greg@endpoint.com>
2012-07-12 12:18:00 -0400

    Always set session_replication_role to replica after customcode runs. As this is a behavior change, bump the minor version, so we are now at 4.5.0. (although I pity the fool that was relying on the previous behavior in any way)

--- Bucardo.pm
+++ Bucardo.pm
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '4.4.9';
+our $VERSION = '4.5.0';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -4184,13 +4184,13 @@ sub start_kid {
         $maindbh->{InactiveDestroy} = 0;
         $sourcedbh->{InactiveDestroy} = 0;
         $targetdbh->{InactiveDestroy} = 0;
-        if ($c->{trigrules}) {
-            if ($source_disable_trigrules eq 'replica') {
-                $sourcedbh->do(q{SET session_replication_role = 'replica'});
-            }
-            if ($target_disable_trigrules eq 'replica') {
-                $targetdbh->do(q{SET session_replication_role = 'replica'});
-            }
+
+        ## Just in case, set session_replication_role back to replica
+        if ($source_disable_trigrules eq 'replica') {
+            $sourcedbh->do(q{SET session_replication_role = 'replica'});
+        }
+        if ($target_disable_trigrules eq 'replica') {
+            $targetdbh->do(q{SET session_replication_role = 'replica'});
         }
         $self->glog("Finished custom code $c->{id}");
         if (length $input->{message}) {
@@ -6261,7 +6261,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.4.9 of Bucardo
+This document describes version 4.5.0 of Bucardo
 
 =head1 WEBSITE
 
