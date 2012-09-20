404132bb4df892757ab2595da34cb8ad1626513a
Greg Sabino Mullane <greg@endpoint.com>
2012-09-20 16:27:37 -0400

    Force time zone to uppercase GMT for old servers.

--- Bucardo.pm
+++ Bucardo.pm
@@ -2542,7 +2542,7 @@ sub start_kid {
             $xdbh->do('SET statement_timeout = 0');
 
             ## Using the same time zone everywhere keeps us sane
-            $xdbh->do('SET TIME ZONE GMT');
+            $xdbh->do(q{SET TIME ZONE 'GMT'});
 
             ## Rare, but allow for tcp fiddling
             if ($config{tcp_keepalives_idle}) { ## e.g. not 0, should always exist
