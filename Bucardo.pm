48ea3919969aad6f0f2641222f6da6ae5bab1d72
Greg Sabino Mullane <greg@endpoint.com>
2012-11-03 15:30:44 -0400

    Bump version to 4.99.6 in preparation for next beta. No HTML files as 'make html' is making too many changes at the moment.

--- Bucardo.pm
+++ Bucardo.pm
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '4.99.5';
+our $VERSION = '4.99.6';
 
 use DBI 1.51;                               ## How Perl talks to databases
 use DBD::Pg 2.0   qw( :async             ); ## How Perl talks to Postgres databases
@@ -8992,7 +8992,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.99.5 of Bucardo
+This document describes version 4.99.6 of Bucardo
 
 =head1 WEBSITE
 
