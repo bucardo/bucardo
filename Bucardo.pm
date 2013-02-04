ab315394ca2f68b43bc96ea86c403e9c08bf642e
Greg Sabino Mullane <greg@endpoint.com>
2013-02-04 11:01:30 -0500

    Bump to 2013

--- Bucardo.pm
+++ Bucardo.pm
@@ -5,7 +5,7 @@
 ##
 ## This script should only be called via the 'bucardo' program
 ##
-## Copyright 2006-2012 Greg Sabino Mullane <greg@endpoint.com>
+## Copyright 2006-2013 Greg Sabino Mullane <greg@endpoint.com>
 ##
 ## Please visit http://bucardo.org for more information
 
@@ -9295,7 +9295,7 @@ Greg Sabino Mullane <greg@endpoint.com>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2005-2012 Greg Sabino Mullane <greg@endpoint.com>.
+Copyright (c) 2005-2013 Greg Sabino Mullane <greg@endpoint.com>.
 
 This software is free to use: see the LICENSE file for details.
 
