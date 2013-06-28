b8aae65747995783e0364794e6cfdd9cb5c301b9
Greg Sabino Mullane <greg@endpoint.com>
2013-06-27 22:15:17 -0400

    Bump version to 4.6.0; bump copyright to 2013

--- Bucardo.pm
+++ Bucardo.pm
@@ -4,7 +4,7 @@
 ##
 ## This script should only be called via the 'bucardo_ctl' program
 ##
-## Copyright 2006-2012 Greg Sabino Mullane <greg@endpoint.com>
+## Copyright 2006-2013 Greg Sabino Mullane <greg@endpoint.com>
 ##
 ## Please visit http://bucardo.org for more information
 
@@ -13,7 +13,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '4.5.1';
+our $VERSION = '4.6.0';
 
 use sigtrap qw( die normal-signals ); ## Call die() on HUP, INT, PIPE, or TERM
 use Config;                           ## Used to map signal names
@@ -6298,7 +6298,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.5.1 of Bucardo
+This document describes version 4.6.0 of Bucardo
 
 =head1 WEBSITE
 
@@ -6340,7 +6340,7 @@ Greg Sabino Mullane <greg@endpoint.com>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2005-2012 Greg Sabino Mullane <greg@endpoint.com>.
+Copyright (c) 2005-2013 Greg Sabino Mullane <greg@endpoint.com>.
 
 This software is free to use: see the LICENSE file for details.
 
