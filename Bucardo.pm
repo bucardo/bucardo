0e546e06186ddc61022d3231d4df3995704d8762
Greg Sabino Mullane <greg@endpoint.com>
2013-11-12 13:13:01 -0500

    Version 4.99.9

--- Bucardo.pm
+++ Bucardo.pm
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '4.99.8';
+our $VERSION = '4.99.9';
 
 use DBI 1.51;                               ## How Perl talks to databases
 use DBD::Pg 2.0   qw( :async             ); ## How Perl talks to Postgres databases
@@ -9382,7 +9382,7 @@ Bucardo - Postgres multi-master replication system
 
 =head1 VERSION
 
-This document describes version 4.99.8 of Bucardo
+This document describes version 4.99.9 of Bucardo
 
 =head1 WEBSITE
 
